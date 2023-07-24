97ee56b2216992035e9c60ed393589564a062f23
sahil <sahilshaikh2286@gmail.com>
2023-07-24 17:54:52 +0530

    some changes

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql2')
 
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "expensetracker"
+});
+
+con.connect(function (err) {
+    if(err)throw err;
+    console.log("connected to database")
+});
 app.get('/',(req, res)=>{
     res.send('hello this is my node server, i am running this node server on aws ec2')
 })
