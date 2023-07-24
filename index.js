184ca650c751046c8461108c04235413c914e738
sahil <sahilshaikh2286@gmail.com>
2023-07-24 16:35:21 +0530

    some changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req, res)=>{
-    res.send('hello this is my node server')
+    res.send('hello this is my node server, i am running this node server on aws ec2')
 })
 
 app.listen(80,()=>{
